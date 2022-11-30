bf3412ec028727be976adc19b1e5353cfe0842c6
Francisca <francisca.orwa@student.moringaschool.com>
2022-11-30 11:03:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+//const { default: fetch } = require("node-fetch");
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response){
+    return response.json();
+  })
+  .then((json)=> renderBooks(json));
+
+  }
   
-}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
